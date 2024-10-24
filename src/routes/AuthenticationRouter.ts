e7712df075e41a41a5b9551aca6690f00d544ed0
asgerbirk <asger.birk@gmail.com>
2024-10-24 11:23:21 +0200

    meerwladt

---
+++ src/routes/AuthenticationRouter.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import { User } from "../types/User.js";
+
+const router = express.Router();
+
+const prisma = new PrismaClient();
+
+router.post("/users", async (req: Request, res: Response) => {});
