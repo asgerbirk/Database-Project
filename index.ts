275f34e6e4ade2925df79c6fcd46699463d54d42
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-11-14 10:48:52 +0100

    Init of mongo connection

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./src/config/swaggerConfig.js";
 import { EmployeeRouter } from "./src/api/routes/EmployeeRouter.js";
 import { connectToDatabase } from "./src/databases/neo4j/neo4jConnection.js";
+import { mongodbConnect } from "./src/databases/mongoDB/mongoConnection.js";
 
 const app = express();
+
 
 app.use(express.json());
 app.use(EmployeeRouter);
 connectToDatabase();
+mongodbConnect();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
