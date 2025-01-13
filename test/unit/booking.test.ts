a46b4c4ea174c5d0de4804382f37eaa134f30b3e
asgerbirk <asger.birk@gmail.com>
2025-01-13 09:51:47 +0100

    invalid and valid

--- test/unit/booking.test.ts
+++ test/unit/booking.test.ts
@@ -3,11 +3,8 @@ import { describe, it, expect } from "vitest";
 import { isClassFull } from "../../src/api/services/Validator";
 
 describe("Test of booking - isClassFull function", () => {
-  // Each object in this array describes:
-  //  - the input (`classData`, `bookingCount`)
-  //  - the expected return (`expected`)
-  //  - a short `description` for logging
   const classFullProvider = [
+    //valind
     {
       description: "bookingCount < MaxParticipants",
       value: { classData: { MaxParticipants: 5 }, bookingCount: 4 },
@@ -19,15 +16,26 @@ describe("Test of booking - isClassFull function", () => {
       expected: undefined,
     },
     {
-      description: "bookingCount > MaxParticipants ",
-      value: { classData: { MaxParticipants: 5 }, bookingCount: 6 },
-      expected: "Class is already full",
-    },
-
-    {
       description: "huge MaxParticipants, bookingCount is smaller",
       value: { classData: { MaxParticipants: 999999999 }, bookingCount: 1 },
       expected: undefined,
+    },
+    {
+      description: "negative bookingCount => not full with our logic",
+      value: { classData: { MaxParticipants: 5 }, bookingCount: -1 },
+      expected: undefined,
+    },
+    {
+      description: "missing MaxParticipants property => not full",
+      value: { classData: {}, bookingCount: 5 },
+      expected: undefined,
+    },
+
+    //invalid
+    {
+      description: "bookingCount > MaxParticipants ",
+      value: { classData: { MaxParticipants: 5 }, bookingCount: 6 },
+      expected: "Class is already full",
     },
 
     {
@@ -40,16 +48,7 @@ describe("Test of booking - isClassFull function", () => {
       value: { classData: { MaxParticipants: 0 }, bookingCount: 1 },
       expected: "Invalid negative capacity or 0",
     },
-    {
-      description: "negative bookingCount => not full with our logic",
-      value: { classData: { MaxParticipants: 5 }, bookingCount: -1 },
-      expected: undefined,
-    },
-    {
-      description: "missing MaxParticipants property => not full",
-      value: { classData: {}, bookingCount: 5 },
-      expected: undefined,
-    },
+
     {
       description: "MaxParticipants is negative ",
       value: { classData: { MaxParticipants: -1 }, bookingCount: 0 },
